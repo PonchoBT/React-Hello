ab4c892ce4d399bfc6dc4d7e05d1f78dd46303da
Alfonso BT <alfonsobalderastorres@gmail.com>
2022-01-29 17:31:43 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import "./styles.css";
 
 export default function App() {
+
+  const list = [
+    {
+    title: 'React',
+    url:'https://reactjs.org/',
+    author:'Jordan Walke',
+    num_comments:3,
+    points: 4,
+    objectID:0,
+    },
+    {
+    title:'Redux',
+    url:'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments:2,
+    points: 5,
+    objectID: 1,
+    },
+    ];
+  
   return (
     <div className="App">
-      <h1>Hello Poncho</h1>
+      {list.map(function(item) {
+      return <li>{item.title}</li>;
+      })}
     </div>
   );
 }
